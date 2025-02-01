9525cea9c694ab1db809484082aa971d476555d4
jreiter <johnreiter@gmail.com>
2025-01-31 23:39:03 -0500

    Add v12 compatibility

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -3,143 +3,150 @@
  * @extends {ActorSheet}
  */
 export class ElectricBastionlandActorSheet extends ActorSheet {
-    /** @override */
-    static get defaultOptions () {
-        return mergeObject(super.defaultOptions, {
-            classes: [
-                "electricbastionland",
-                "sheet",
-                "actor"
-            ],
-            template: "systems/electricbastionland/templates/actor/actor-sheet.html",
-            width: 500,
-            height: 600,
-            tabs: [
-                {
-                    navSelector: ".sheet-tabs",
-                    contentSelector: ".sheet-body",
-                    initial: "description",
-                },
-            ],
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["electricbastionland", "sheet", "actor"],
+      template: "systems/electricbastionland/templates/actor/actor-sheet.html",
+      width: 500,
+      height: 600,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "description",
+        },
+      ],
+    });
+  }
+
+  /* -------------------------------------------- */
+  /** @override */
+  getData() {
+    const context = super.getData();
+
+    // Add system data reference for template
+    context.systemData = this.actor.system;
+    context.system = this.actor.system;
+
+    return context;
+  }
+
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
+
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
+
+    // Handle rollable items and attributes
+    html.find(".rollable").on("click", this._onItemRoll.bind(this));
+
+    // Update Inventory Item
+    html.find(".item-edit").click((ev) => {
+      const li = $(ev.currentTarget).parents(".item")[0];
+      const item = this.actor.items.get(li.dataset.itemId);
+      item.sheet.render(true);
+    });
+
+    // Delete Inventory Item
+    html.find(".item-delete").click((ev) => {
+      const li = $(ev.currentTarget).parents(".item")[0];
+      this.actor.deleteEmbeddedDocuments("Item", [li.dataset.itemId]);
+    });
+
+    html.find(".item-create").click(this._onItemCreate.bind(this));
+
+    // Add draggable for macros.
+    html.find(".attributes a.attribute-roll").each((i, a) => {
+      a.setAttribute("draggable", true);
+      a.addEventListener(
+        "dragstart",
+        (ev) => {
+          let dragData = ev.currentTarget.dataset;
+          ev.dataTransfer.setData("text/plain", JSON.stringify(dragData));
+        },
+        false,
+      );
+    });
+
+    // Rest restores HP
+    html.find(".rest").click(async (ev) => {
+      // Someone DEPRIVED of a crucial need (e.g.
+      // food,water or warmth) cannot benefit from RESTS
+      if (!this.actor.system.deprived) {
+        await this.actor.update({
+          "system.hp.value": this.actor.system.hp.max,
         });
+      }
+    });
+
+    html.find(".restore").click(async (ev) => {
+      await this.actor.update({
+        "system.abilities.STR.value": this.actor.system.abilities.STR.max,
+      });
+      await this.actor.update({
+        "system.abilities.DEX.value": this.actor.system.abilities.DEX.max,
+      });
+      await this.actor.update({
+        "system.abilities.CHA.value": this.actor.system.abilities.CHA.max,
+      });
+    });
+
+    html.find(".luck").click(async (ev) => {
+      let roll = new Roll("1d6");
+      await roll.evaluate();
+    });
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  _onItemCreate(event) {
+    event.preventDefault();
+    const header = event.currentTarget;
+    // Get the type of item to create.
+    const type = header.dataset.type;
+    // Grab any data associated with this control.
+    const data = duplicate(header.dataset);
+    // Initialize a default name.
+    const name = `New ${type.capitalize()}`;
+    // Prepare the item object.
+    const itemData = {
+      name: name,
+      type: type,
+      system: data,
+    };
+    // Remove the type from the dataset since it's in the itemData.type prop.
+    delete itemData.system["type"];
+
+    // Finally, create the item!
+    return this.actor.createEmbeddedDocuments("Item", [itemData]);
+  }
+
+  /**
+   * Handle clickable rolls.
+   * @param {Event} event   The originating click event
+   * @private
+   */
+  async _onItemRoll(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const dataset = element.dataset;
+
+    if (dataset.roll) {
+      let roll = new Roll(dataset.roll, this.actor.system);
+      let label = dataset.label ? `Rolling ${dataset.label}` : "";
+      await roll.evaluate();
+
+      roll.toMessage({
+        speaker: ChatMessage.getSpeaker({ actor: this.actor }),
+        flavor: label,
+      });
     }
-
-    /* -------------------------------------------- */
-    /** @override */
-    getData () {
-        const context = super.getData();
-        context.systemData = context.data.data;
-
-        return context;
-    }
-
-    /** @override */
-    activateListeners (html) {
-        super.activateListeners(html);
-
-        // Everything below here is only needed if the sheet is editable
-        if ( !this.options.editable ) return;
-
-        // Handle rollable items and attributes
-        html.find(".rollable").on("click", this._onItemRoll.bind(this));
-
-        // Update Inventory Item
-        html.find('.item-edit').click(ev => {
-            const li = $(ev.currentTarget).parents(".item")[0];
-            const item = this.actor.items.get(li.dataset.itemId);
-            item.sheet.render(true);
-        });
-
-        // Delete Inventory Item
-        html.find('.item-delete').click(ev => {
-            const li = $(ev.currentTarget).parents(".item")[0];
-            this.actor.deleteEmbeddedDocuments("Item", [li.dataset.itemId]);
-        });
-
-        html.find(".item-create").click(this._onItemCreate.bind(this));
-
-        // Add draggable for macros.
-        html.find(".attributes a.attribute-roll").each((i, a) => {
-            a.setAttribute("draggable", true);
-            a.addEventListener("dragstart", ev => {
-                let dragData = ev.currentTarget.dataset;
-                ev.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-            }, false);
-        });
-
-        // Rest restores HP
-        html.find('.rest')
-            .click(async ev => {
-                // Someone DEPRIVED of a crucial need (e.g.
-                // food,water or warmth) cannot benefit from RESTS
-                if (!this.actor.data.data.deprived) {
-                    await this.actor.update({'data.hp.value': this.actor.data.data.hp.max});
-                }
-            });
-
-        html.find('.restore')
-            .click(async ev => {
-                await this.actor.update({'data.abilities.STR.value': this.actor.data.data.abilities.STR.max});
-                await this.actor.update({'data.abilities.DEX.value': this.actor.data.data.abilities.DEX.max});
-                await this.actor.update({'data.abilities.CHA.value': this.actor.data.data.abilities.CHA.max});
-            });
-
-        html.find('.luck')
-            .click(ev => {
-                let roll = new Roll('1d6');
-                roll.roll();
-            });
-    }
-
-    /* -------------------------------------------- */
-
-    /**
-     * Handle creating a new Owned Item for the actor using initial data defined in the HTML dataset
-     * @param {Event} event   The originating click event
-     * @private
-     */
-    _onItemCreate (event) {
-        event.preventDefault();
-        const header = event.currentTarget;
-        // Get the type of item to create.
-        const type = header.dataset.type;
-        // Grab any data associated with this control.
-        const data = duplicate(header.dataset);
-        // Initialize a default name.
-        const name = `New ${type.capitalize()}`;
-        // Prepare the item object.
-        const itemData = {
-            name: name,
-            type: type,
-            data: data,
-        };
-        // Remove the type from the dataset since it's in the itemData.type prop.
-        delete itemData.data["type"];
-
-        // Finally, create the item!
-        return this.actor.createEmbeddedDocuments("Item", [itemData]);
-    }
-
-    /**
-     * Handle clickable rolls.
-     * @param {Event} event   The originating click event
-     * @private
-     */
-    _onItemRoll (event) {
-        event.preventDefault();
-        const element = event.currentTarget;
-        const dataset = element.dataset;
-
-        if (dataset.roll) {
-            let roll = new Roll(dataset.roll, this.actor.data.data);
-            let label = dataset.label ? `Rolling ${dataset.label}` : '';
-            roll.roll({ async: false });
-
-            roll.toMessage({
-                speaker: ChatMessage.getSpeaker({actor: this.actor}),
-                flavor: label,
-            });
-        }
-    }
-
+  }
 }
--- module/actor/actor.js
+++ module/actor/actor.js
@@ -3,60 +3,37 @@
  * @extends {Actor}
  */
 export class ElectricBastionlandActor extends Actor {
-    /**
-     * Augment the basic actor data with additional dynamic data.
-     */
-    prepareData () {
-        super.prepareData();
-        this.data.data.groups = this.data.data.groups || {};
-        this.data.data.attributes = this.data.data.attributes || {};
+  /**
+   * Augment the basic actor data with additional dynamic data.
+   */
+  prepareData() {
+    super.prepareData();
+    this.system.groups = this.system.groups || {};
+    this.system.attributes = this.system.attributes || {};
 
-        if (this.data.type === 'character') this._prepareCharacterData(this.data);
+    if (this.type === "character") this._prepareCharacterData();
+  }
+
+  /**
+   * Prepare Character type specific data
+   */
+  _prepareCharacterData() {
+    // sum all armour for equipped items
+    this.system.armour = this.items
+      .map((item) => (item.system.equipped ? item.system.armour : 0))
+      .reduce((a, b) => a + b, 0);
+
+    // check if more than two items are bulky
+    this.system.tooBulky =
+      this.items.filter((item) => item.system.bulky).length > 2;
+
+    if (this.system.tooBulky) {
+      this.system.hp.value = 0;
     }
+  }
 
-    /**
-     * Prepare Character type specific data
-     */
-    _prepareCharacterData (actorData) {
-        const data = actorData.data;
-
-        // sum all armour for equipped items
-        data.armour = actorData
-            .items
-            .map(
-                item => item.data.data.equipped ? item.data.data.armour : 0
-            )
-            .reduce(
-                (a, b) => a + b, 0
-            )
-
-        // check if more than two items are bulky
-        data.tooBulky = actorData
-            .items
-            .filter(
-                item => item.data.data.bulky
-            )
-            .length > 2;
-
-        if (data.tooBulky) {
-            data.hp.value = 0;
-        }
-
-    }
-
-    /** @override */
-    getRollData () {
-        return super.getRollData();
-    }
-
-    /** @override */
-    deleteOwnedItem (itemId) {
-        const item = this.getOwnedItem(itemId);
-
-        if (item.data.data.quantity > 1) {
-            item.data.data.quantity--;
-        } else {
-            super.deleteOwnedItem(itemId);
-        }
-    }
+  /** @override */
+  getRollData() {
+    return super.getRollData();
+  }
 }

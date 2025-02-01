bf505d72fec2d9642b2855800d3e6f8911a4b065
jreiter <johnreiter@gmail.com>
2025-01-31 23:39:03 -0500

    Add v12 compatibility

--- module/item/item-sheet.js
+++ module/item/item-sheet.js
@@ -3,51 +3,51 @@
  * @extends {ItemSheet}
  */
 export class ElectricBastionlandItemSheet extends ItemSheet {
+  /** @override */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: ["electricbastionland", "sheet", "item"],
+      width: 420,
+      height: 440,
+    });
+  }
 
-    /** @override */
-    static get defaultOptions () {
-        return mergeObject(super.defaultOptions, {
-            classes: ["electricbastionland", "sheet", "item"],
-            width: 420,
-            height: 440
-        });
-    }
+  /** @override */
+  get template() {
+    const path = "systems/electricbastionland/templates/item";
+    return `${path}/item-sheet.html`;
+  }
 
-    /** @override */
-    get template () {
-        const path = "systems/electricbastionland/templates/item";
-        return `${path}/item-sheet.html`;
-    }
+  /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
+  /** @override */
+  getData() {
+    const context = super.getData();
+    context.systemData = this.item.system;
+    context.system = this.item.system;
+    return context;
+  }
 
-    /** @override */
-    getData () {
-        const context = super.getData();
-        context.systemData = context.data.data;
-        return context;
-    }
+  /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
+  /** @override */
+  setPosition(options = {}) {
+    const position = super.setPosition(options);
+    const sheetBody = this.element.find(".sheet-body");
+    const bodyHeight = position.height - 192;
+    sheetBody.css("height", bodyHeight);
+    return position;
+  }
 
-    /** @override */
-    setPosition (options = {}) {
-        const position = super.setPosition(options);
-        const sheetBody = this.element.find(".sheet-body");
-        const bodyHeight = position.height - 192;
-        sheetBody.css("height", bodyHeight);
-        return position;
-    }
+  /* -------------------------------------------- */
 
-    /* -------------------------------------------- */
+  /** @override */
+  activateListeners(html) {
+    super.activateListeners(html);
 
-    /** @override */
-    activateListeners (html) {
-        super.activateListeners(html);
+    // Everything below here is only needed if the sheet is editable
+    if (!this.options.editable) return;
 
-        // Everything below here is only needed if the sheet is editable
-        if (!this.options.editable) return;
-
-        // Roll handlers, click handlers, etc. would go here.
-    }
+    // Roll handlers, click handlers, etc. would go here.
+  }
 }
